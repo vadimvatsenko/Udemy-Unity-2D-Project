40387ea7f2ae79ff27022b4a335038e61c413e35
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2023-11-20 15:14:16 +0200

    add 89.90.91.92.93.94 lessons

--- Assets/Scripts/LeverArm.cs
+++ Assets/Scripts/LeverArm.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class LeverArm : MonoBehaviour
 {
     private Finish _finish; // создаем обьект финиш из скрипта финиш
+    private Animator _animator;
     void Start()
     {
         _finish = GameObject.FindGameObjectWithTag("Finish").GetComponent<Finish>(); // получаем доступ к обьекту с тегом финиш и получаем с него обьект финиш
+        _animator = GetComponent<Animator>();
     }
     public void ActivateLevelArm()
     {
+        _animator.SetTrigger("activate");
         //finish.FinishLevel(); // активируем финиш
         _finish.Activate();
     }
